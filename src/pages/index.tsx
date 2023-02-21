b228b63f68048bc6fb8e3eee883c217643f22a6e
Christopher Bohl <chrisrbohl@gmail.com>
2023-02-21 15:26:05 -0600

    Styles index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Native WebRTC API with NextJS</title>
+        <title>Vif: Video Hangout and GIFs</title>
         <meta
           name='description'
           content='Use Native WebRTC API for video conferencing'
@@ -23,16 +23,19 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Lets join a room!</h1>
+        <h1 className='text-lime-500 bold'>
+          Welcome to Vif: Video Hangout and GIFs!
+        </h1>
+        <h2>Select a room now!</h2>
         <input
           onChange={(e) => setRoomName(e.target.value)}
           value={roomName}
-          className={styles["room-name"]}
+          className='bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500'
         />
         <button
           onClick={joinRoom}
           type='button'
-          className={styles["join-room"]}
+          className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'
         >
           Join Room
         </button>
