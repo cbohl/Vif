47c32fbbe2797c40b2f45329305984b58a43c657
Christopher Bohl <chrisrbohl@gmail.com>
2023-03-01 09:54:36 -0600

    Improves type checking of giphy api page.

--- src/pages/api/giphy.ts
+++ src/pages/api/giphy.ts
@@ -1,21 +1,19 @@
-type Data = {
-  name: string;
-};
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function serverSideCall(req: any, res: any) {
+export default async function serverSideCall(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const apiUrl = "https://api.giphy.com/v1/gifs/search";
-  const apiKey = process.env.GIPHY_API_KEY;
+  const apiKey: string = process.env.GIPHY_API_KEY as string;
 
-  const searchTerm = req.query["gifSearch"];
+  const searchTerm: string = req.query["gifSearch"] as string;
   const searchLimit = 9;
-
-  let fetchResultJson;
 
   const fetchResult = await fetch(
     `${apiUrl}?api_key=${apiKey}&q=${searchTerm}&limit=${searchLimit}`
   );
-
-  fetchResultJson = await fetchResult.json();
+  const fetchResultJson: JSON = (await fetchResult.json()) as JSON;
 
   res.status(200).json(fetchResultJson);
 }
