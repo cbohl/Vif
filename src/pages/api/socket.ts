74077a174b79240c3310292ccbbbd3f4c5854e29
Christopher Bohl <chrisrbohl@gmail.com>
2023-03-04 21:00:08 -0600

    Improves type checking on socket API page.

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,79 +1,137 @@
-/* eslint-disable */
+// /* eslint-disable */
 
-import { Server } from "socket.io";
+import type { Server as HTTPServer } from "http";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Server as IOServer } from "socket.io";
+import type { Socket as NetSocket } from "net";
+import { Server, Socket } from "socket.io";
 
-const SocketHandler = (req: any, res: any) => {
-  if (res.socket.server.io) {
-    console.log("Socket is already attached");
-    return res.end();
+interface SocketServer extends HTTPServer {
+  io?: IOServer | undefined;
+}
+
+interface SocketWithIO extends NetSocket {
+  server: SocketServer;
+}
+
+interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithIO;
+}
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+interface SocketServer extends HTTPServer {
+  io?: IOServer | undefined;
+}
+
+interface SocketWithIO extends NetSocket {
+  server: SocketServer;
+}
+
+interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithIO;
+}
+
+const SocketHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
+  if (res.socket) {
+    if (res.socket.server.io) {
+      console.log("Socket is already attached");
+      return res.end();
+    }
   }
 
-  const io = new Server(res.socket.server);
-  res.socket.server.io = io;
+  if (res.socket) {
+    // const io = new Server(res.socket.server);
+    const io = new Server<
+      ClientToServerEvents,
+      ServerToClientEvents,
+      InterServerEvents,
+      SocketData
+    >(res.socket.server);
 
-  io.on("connection", (socket) => {
-    console.log(`User Connected :${socket.id}`);
+    res.socket.server.io = io;
 
-    socket.on("set-gif-to-server", (gifUrl, roomName) => {
-      console.log("sending gif", gifUrl); // world
-      socket.broadcast.to(roomName).emit("new-gif-from-server", gifUrl);
+    io.on("connection", (socket: Socket) => {
+      console.log(`User Connected :${socket.id}`);
+
+      socket.on("set-gif-to-server", (gifUrl, roomName: string) => {
+        console.log("sending gif", gifUrl); // world
+        socket.broadcast.to(roomName).emit("new-gif-from-server", gifUrl);
+      });
+
+      socket.on("new-peer-to-server", (roomName: string) => {
+        console.log("new peer to server!");
+        socket.broadcast.to(roomName).emit("new-peer-from-server");
+      });
+
+      // Triggered when a peer hits the join room button.
+      socket.on("join", (roomName: string) => {
+        const { rooms } = io.sockets.adapter;
+        const room = rooms.get(roomName);
+
+        // room == undefined when no such room exists.
+        if (room === undefined) {
+          void socket.join(roomName);
+          socket.emit("created");
+        } else if (room.size === 1) {
+          // room.size == 1 when one person is inside the room.
+          void socket.join(roomName);
+          socket.emit("joined");
+        } else {
+          // when there are already two people inside the room.
+          socket.emit("full");
+        }
+        console.log(rooms);
+      });
+
+      // Triggered when the person who joined the room is ready to communicate.
+      socket.on("ready", (roomName: string) => {
+        socket.broadcast.to(roomName).emit("ready"); // Informs the other peer in the room.
+      });
+
+      // Triggered when server gets an icecandidate from a peer in the room.
+      socket.on(
+        "ice-candidate",
+        (candidate: RTCIceCandidate, roomName: string) => {
+          console.log(candidate);
+          socket.broadcast.to(roomName).emit("ice-candidate", candidate); // Sends Candidate to the other peer in the room.
+        }
+      );
+
+      // Triggered when server gets an offer from a peer in the room.
+      socket.on("offer", (offer, roomName: string) => {
+        socket.broadcast.to(roomName).emit("offer", offer); // Sends Offer to the other peer in the room.
+      });
+
+      // Triggered when server gets an answer from a peer in the room.
+      socket.on("answer", (answer, roomName: string) => {
+        socket.broadcast.to(roomName).emit("answer", answer); // Sends Answer to the other peer in the room.
+      });
+
+      socket.on("leave", (roomName: string) => {
+        void socket.leave(roomName);
+        socket.broadcast.to(roomName).emit("leave");
+      });
     });
-
-    socket.on("new-peer-to-server", (roomName) => {
-      console.log("new peer to server!");
-      socket.broadcast.to(roomName).emit("new-peer-from-server");
-    });
-
-    // Triggered when a peer hits the join room button.
-    socket.on("join", (roomName) => {
-      const { rooms } = io.sockets.adapter;
-      const room = rooms.get(roomName);
-
-      // room == undefined when no such room exists.
-      if (room === undefined) {
-        socket.join(roomName);
-        socket.emit("created");
-      } else if (room.size === 1) {
-        // room.size == 1 when one person is inside the room.
-        socket.join(roomName);
-        socket.emit("joined");
-      } else {
-        // when there are already two people inside the room.
-        socket.emit("full");
-      }
-      console.log(rooms);
-    });
-
-    // Triggered when the person who joined the room is ready to communicate.
-    socket.on("ready", (roomName) => {
-      socket.broadcast.to(roomName).emit("ready"); // Informs the other peer in the room.
-    });
-
-    // Triggered when server gets an icecandidate from a peer in the room.
-    socket.on(
-      "ice-candidate",
-      (candidate: RTCIceCandidate, roomName: string) => {
-        console.log(candidate);
-        socket.broadcast.to(roomName).emit("ice-candidate", candidate); // Sends Candidate to the other peer in the room.
-      }
-    );
-
-    // Triggered when server gets an offer from a peer in the room.
-    socket.on("offer", (offer, roomName) => {
-      socket.broadcast.to(roomName).emit("offer", offer); // Sends Offer to the other peer in the room.
-    });
-
-    // Triggered when server gets an answer from a peer in the room.
-    socket.on("answer", (answer, roomName) => {
-      socket.broadcast.to(roomName).emit("answer", answer); // Sends Answer to the other peer in the room.
-    });
-
-    socket.on("leave", (roomName) => {
-      socket.leave(roomName);
-      socket.broadcast.to(roomName).emit("leave");
-    });
-  });
-  return res.end();
+    return res.end();
+  }
 };
 
 export default SocketHandler;
